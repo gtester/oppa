ccdbbf115a57245c1c16dd0b343d7e8ae2906388
gtester <git-tester@git.ru>
2013-08-02 13:51:04 +0400

    What to do

--- gtest/src/main/java/Main.java
+++ gtest/src/main/java/Main.java
@@ -2,6 +2,7 @@
 // Hello oppa
 public class Main {
     public static void main(String[] args) {
+        //TODO: what to do?
         Volkswagen volkswagen = new Volkswagen();
         for(int i=0; i<20; i++) {
             volkswagen.add(new Clown());
