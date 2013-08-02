e5a528e5fbba5d2533343d372cf6f12fb5a4ad82
gtester <git-tester@git.ru>
2013-08-02 13:04:35 +0400

    Another commit

--- gtest/src/main/java/ClownVolkswagen.java
+++ gtest/src/main/java/ClownVolkswagen.java
@@ -2,13 +2,7 @@
 public class ClownVolkswagen extends Volkswagen {
     private static final int CAPACITY = 500;
 
-    @Override
-    public synchronized void add(Clown clown) {
-        super.add(clown);    //To change body of overridden methods use File | Settings | File Templates.
-    }
 
-    @Override
-    public synchronized void done() {
-        super.done();    //To change body of overridden methods use File | Settings | File Templates.
-    }
+
+
 }
