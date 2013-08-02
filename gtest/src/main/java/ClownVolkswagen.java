ba3136b75c45aa896edaab3a8e17ede8c7f17964
gtester <git-tester@git.ru>
2013-08-02 12:32:10 +0400

    message
    Second Attempt

--- gtest/src/main/java/ClownVolkswagen.java
+++ gtest/src/main/java/ClownVolkswagen.java
@@ -2,5 +2,8 @@
 public class ClownVolkswagen extends Volkswagen {
     private static final int CAPACITY = 500;
 
-
+    @Override
+    public synchronized void add(Clown clown) {
+        super.add(clown);    //To change body of overridden methods use File | Settings | File Templates.
+    }
 }
