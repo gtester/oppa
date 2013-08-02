d189d494fb261ff538169028a6a71b84fcea296e
gtester <git-tester@git.ru>
2013-08-02 12:55:28 +0400

    Playing with wagens

--- gtest/src/main/java/Main.java
+++ gtest/src/main/java/Main.java
@@ -1,7 +1,7 @@
 
 public class Main {
     public static void main(String[] args) {
-        Volkswagen volkswagen = new ClownVolkswagen();
+        Volkswagen volkswagen = new Volkswagen();
         for(int i=0; i<20; i++) {
             volkswagen.add(new Clown());
         }
